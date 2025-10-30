4c6e36a864d4b9cba020ee1e0eab7ff9801866c9
emerald067 <eawoke@byupathway.edu>
2025-10-30 12:52:40 +0100

    week01/Excercise1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //get the gradepercentage from the user.
+        Console.Write("Please enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+
+        string letter = "";
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // get the last digit.
+        int lastDigit = gradePercentage % 10;
+        string sign = "";
+        if (gradePercentage >= 7)
+        {
+            sign = "+";
+        }
+        else if (gradePercentage <= 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        // handle exceptions (A+, F+ and F-)
+        if (gradePercentage >= 90)
+        {
+            sign = "";
+        }
+        else if (gradePercentage <= 50)
+        {
+            sign = "";
+        }
+        
+        // display the grade
+
+        Console.WriteLine($"Your grade is: {letter}{sign}. ");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you have not passed the course. Better luck next time!");
+        }
     }
 }
