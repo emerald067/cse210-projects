2b748532e3f5e3d8f5c798cea284801682a8f4f3
emerald067 <eawoke@byupathway.edu>
2025-10-30 15:11:27 +0100

    c# week01 excercise 3 (loops)

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        string playAgain = "yes";
+        while (playAgain == "yes")
+        {
+            int guess = -1;
+            int numberOfGuesses = 0;
+            while (guess != number)
+            {
+                if (guess != number)
+                {
+                    numberOfGuesses++;
+                }
+                Console.Write("What is your guess? ");
+                string magicGuess = Console.ReadLine();
+                guess = int.Parse(magicGuess);
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You made {numberOfGuesses} guess(es). ");
+                    Console.Write("Would you want to play again (yes/no)? ");
+                    playAgain = Console.ReadLine();
+                }
+            }
+        }
     }
 }
