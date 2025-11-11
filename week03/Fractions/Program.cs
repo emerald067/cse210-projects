dce2396d3736f6e0faca06fb1163d17801386a31
emerald067 <eawoke@byupathway.edu>
2025-11-11 19:45:44 +0100

    week 03 activity (Encapsulation)

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -2,8 +2,22 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"The fraction is {fraction1.GetFractionString()}");
+        Console.WriteLine($"The decimal value is {fraction1.GetDecimalValue()}");
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine($"The fraction is {fraction2.GetFractionString()}");
+        Console.WriteLine($"The decimal value is {fraction2.GetDecimalValue()}");
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine($"The fraction is {fraction3.GetFractionString()}");
+        Console.WriteLine($"The decimal value is {fraction3.GetDecimalValue()}");
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine($"The fraction is {fraction4.GetFractionString()}");
+        Console.WriteLine($"The decimal value is {fraction4.GetDecimalValue()}");
     }
 }
